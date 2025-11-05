c5199f3a9597f7cec314eaa29b17329c336a477a
SwingSimon <SwingSimon@SWINGS-LAPTOP>
2025-11-05 00:19:15 -0500

    Another coding pass

--- Game.cs
+++ Game.cs
@@ -12,12 +12,52 @@ namespace MohawkGame2D
     {
         // Place your variables here:
 
+        // Vector 2 = Reapeat as needed for platform positions
+        Vector2[] platformPos = [new Vector2(),  ]
+
+        // vector2 = repeast for platform placements
+        Vector2[] platformSize = [new Vector2(), ]
+
+        // platform array for number of platforms (tbd)
+        platform[] platforms = new platform[]
+
+        player Skeleman;
+        badPlatform deadBlock;
+        goal endSqaure;
+
+
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Skeleman Escapes");
+            Window.SetSize(500, 500);
+            Window.TargetFPS = 60;
+
+            //Initialize all the game objects here
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                platforms[i] = new platform();
+                platforms[i].position = platformPos[i];
+                platforms[i].size = platformSize[i];
+                platforms[i].Setup();
+
+            }
+
+            Skeleman = new player();
+            Skeleman.Setup();
+
+
+            deadBlock = new badPlatform();
+            deadBlock.position = new Vector2();
+            deadBlock.Setup();
+                        
+
+            endSquare = new goal();
+            endGoal.position = new Vector2();
+            endGoal.Setup();
 
         }
 
---
+++ player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class player
+    {
+        // Variables for collision detection
+        public float leftEdge = 0.0f;
+        public float rightEdge = 0.0f;
+        public float topEdge = 0.0f;
+        public float bottomEdge = 0.0f;
+
+
+        // These are the private variables
+
+        Vector2 position;
+        Vector2 veolcity;
+        Vector2 gravity = new Vector2(0, 1500);
+        Vector2 size = new Vector2(30, 30);
+
+        float speed = 200.0f;
+
+        //Potential walk cycle with a few pngs
+        Vector2 lastposition = new Vector2();
+
+        public void Setup()
+        { 
+            velocity = new Vector2(0, 0);
+            position = new Vector2(0, 0);
+            walkLeft = Graphics.LoadTexture("Textures/Skele-Left-Walk.png");
+            walkRight = Graphics.LoadTexture("Textures/SKele-Right-Walk.png");
+        }
+    }
+}
